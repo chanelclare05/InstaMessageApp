ebb2c0741b111d4ebc2885b5f9c0d7bef66c60b5
chanel donahoe <cdonahoe05@gmail.com>
2021-11-07 19:46:30 +1100

    added Message resource to the application - back end build

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,9 @@
 # User.create(username: "bob", password:"password")
 # User.create(username: "george", password:"password")
 # User.create(username: "tiffany", password:"password")
+
+# Message.create(body: "Hello", user_id: 2)
+# Message.create(body: "Hola", user_id: 3)
+# Message.create(body: "Ciao", user_id: 4)
+# Message.create(body: "Konnichiwa", user_id: 5)
+# Message.create(body: "Salut", user_id: 2)
