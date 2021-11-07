17efea43d2b0346bddb213dfe16a66e0652be40c
chanel donahoe <cdonahoe05@gmail.com>
2021-11-07 16:40:34 +1100

    created nav bar from semantic ui

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '@doabit/semantic-ui-sass'
+
 
 Rails.start()
 Turbolinks.start()
